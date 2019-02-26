2bb9047885590e876c8f0d3d609fd913ec3b3d5f
diveresque <katiepringles@yahoo.com.au>
2019-02-26 21:59:30 +1100

    Test change to Map object
    remove jQuery syntax

--- public/qiblih-update.js
+++ public/qiblih-update.js
@@ -63,7 +63,7 @@ Qibla.startMap = function() {
 Qibla.initMap = function() {
 
 	this.map = new google.maps.Map(
-		$("#QMap"), {
+		document.getElementById('QMap'), {
 		center: this.home,
 		zoom: this.zoom,
 		mapTypeId: google.maps.MapTypeId.TERRAIN	
