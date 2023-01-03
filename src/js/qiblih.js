61236c9fda39d3244ec82eb11ce507f0adb20517
William Cooley <git@wtip.net>
2023-01-02 21:00:11 -0500

    compass initial test

--- src/js/qiblih.js
+++ src/js/qiblih.js
@@ -148,3 +148,42 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+// Compass stuff
+const compassCircle = document.querySelector(".compass-circle");
+const startBtn = document.querySelector(".start-btn");
+const myPoint = document.querySelector(".my-point");
+let compass;
+const isIOS = (
+  navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
+  navigator.userAgent.match(/AppleWebKit/)
+);
+
+function init() {
+  startBtn.addEventListener("click", startCompass);
+}
+
+function startCompass() {
+  if (isIOS) {
+    console.log("is IOS");
+    DeviceOrientationEvent.requestPermission()
+      .then((response) => {
+        if (response === "granted") {
+          window.addEventListener("deviceorientation", handler, true);
+        } else {
+          alert("has to be allowed!");
+        }
+      })
+      .catch(() => alert("not supported"));
+  } else {
+    window.addEventListener("deviceorientationabsolute", handler, true);
+    console.log("is not IOS");
+  }
+}
+
+function handler(e) {
+  compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
+  compassCircle.style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
+}
+
+init();
