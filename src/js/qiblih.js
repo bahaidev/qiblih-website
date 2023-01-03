4f03d0f2c842facdc20a61d5b6e72a6e8e4be8fe
William Cooley <git@wtip.net>
2023-01-03 16:35:08 -0500

    more compass stuff

--- src/js/qiblih.js
+++ src/js/qiblih.js
@@ -185,8 +185,8 @@ function startCompass() {
 
 function compHandler(e) {
   compass = Math.round(e.webkitCompassHeading || Math.abs(e.alpha - 360));
-  let target = compass - targetBearing - 360;
-  compassCircle.style.transform = `translate(-50%, -50%) rotate(${-target}deg)`;
+  let target = compass + targetBearing - 360;
+  compassCircle.style.transform = `translate(-50%, -50%) rotate(${target}deg)`;
 
   if (target < 15 || target > 345) {
     triangle.style.opacity = 1;
@@ -194,7 +194,7 @@ function compHandler(e) {
     triangle.style.opacity = 0;
   }
 
-  document.querySelector("#test").innerHTML = `Current Bearing: ${compass} <br> Target Bearing: ${targetBearing}`;
+  document.querySelector("#test").innerHTML = `Current Bearing: ${compass} <br> Needed Correction: ${target} <br> Target Bearing: ${targetBearing}`;
 }
 
 initCompass();
