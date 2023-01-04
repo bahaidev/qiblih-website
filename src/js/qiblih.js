f7ba6e058404998739972291cbf2318d119da119
William Cooley <git@wtip.net>
2023-01-03 20:25:40 -0500

    compass work

--- src/js/qiblih.js
+++ src/js/qiblih.js
@@ -185,7 +185,7 @@ function startCompass() {
 
 function compHandler(e) {
   compass = Math.round(e.webkitCompassHeading || Math.abs(e.alpha - 360));
-  let target = compass + targetBearing - 360;
+  let target = (360 + targetBearing - compass) % 360;
   compassCircle.style.transform = `translate(-50%, -50%) rotate(${target}deg)`;
 
   if (target < 15 || target > 345) {
